aaaa0da8a6d55994853744b3ed7f447f4cb17a46
Jesse Smith <jesse.lee.smith@gmail.com>
2021-09-08 18:38:09 -0700

    commit tree befce860db89668acef8049d2de8f41595563d0d

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,25 @@
 # Enter your procedural solution here!
+def largest_prime_factor(number)
+    factors = []
+
+    while number % 2 == 0
+        factors.push(2)
+        number /= 2
+    end
+
+    i = 3
+
+    while i <= Math.sqrt(number)
+        while number % i == 0
+            factors.push(i)
+            number /= i
+        end
+
+        i +=  2
+    end
+
+    if( number > 2)
+        factors.push(number)
+    end
+    factors.pop()
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,32 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+    def initialize(input)
+    @input = input
+    end
+
+    def number()
+        number = @input
+        factors = []
+    
+        while number % 2 == 0
+            factors.push(2)
+            number /= 2
+        end
+    
+        i = 3
+    
+        while i <= Math.sqrt(number)
+            while number % i == 0
+                factors.push(i)
+                number /= i
+            end
+    
+            i +=  2
+        end
+    
+        if( number > 2)
+            factors.push(number)
+        end
+        factors.pop()
+    end
+end
